9feb66361012832dd20da2a39f8ec46e808edeb8
Bethany Fulcher <fulcherbethany@gmail.com>
2021-01-12 20:24:13 -0800

    added all prompts and functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const employees = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -15,6 +16,11 @@ const render = require("./lib/htmlRenderer");
 // and to create objects for each team member (using the correct classes as blueprints!)
 function askManager() {
     inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of your company?',
+            name: "companyName"
+        },
         {
             type: 'input',
             message: 'What is your name?',
@@ -35,16 +41,11 @@ function askManager() {
             message: 'What is your office Number?',
             name: "managerOffice"
         },
-        {
-            type: 'list',
-            message: 'What kind of employee would you like to add?',
-            name: "employeeRole",
-            choices: ['Engineer', 'Intern']
-        },
     ]).then((answers) => {
-        console.log(answers.managerName)
         const newManager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice)
         console.log(newManager)
+        employees.push(newManager)
+        choose()
     })
 }
 
@@ -53,10 +54,89 @@ function choose() {
         {
             type: 'list',
             message: 'What kind of employee would you like to add?',
-            name: "employeeRole",
-            choices: ['Engineer', 'Intern']
+            name: "choice",
+            choices: ['Engineer', 'Intern', 'Done']
         },
-    ])
+    ]).then(({ choice }) => {
+        switch (choice) {
+            case "Engineer":
+                addEngineer()
+                break;
+            case "Intern":
+                addIntern()
+                break;
+            case "Done":
+                console.log("Creating an html now!")
+                createHTML()
+                break;
+            default:
+                break;
+        }
+    })
+}
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: `What is this engineer's name?`,
+            name: "engineerName"
+        },
+        {
+            type: 'number',
+            message: `What is this engineer's id number?`,
+            name: "engineerId"
+        },
+        {
+            type: 'input',
+            message: `What is this engineer's email?`,
+            name: "engineerEmail"
+        },
+        {
+            type: 'input',
+            message: `What is this engineer's GitHub username?`,
+            name: "engineerGitHub"
+        },
+    ]).then((answers) => {
+        const newEngineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGitHub)
+        console.log(newEngineer)
+        employees.push(newEngineer);
+        choose()
+    })
+}
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: `What is this intern's name?`,
+            name: "internName"
+        },
+        {
+            type: 'number',
+            message: `What is this intern's id number?`,
+            name: "internId"
+        },
+        {
+            type: 'input',
+            message: `What is this intern's email?`,
+            name: "internEmail"
+        },
+        {
+            type: 'input',
+            message: `What is this intern's school?`,
+            name: "internSchool"
+        },
+    ]).then((answers) => {
+        const newIntern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
+        console.log(newIntern)
+        employees.push(newIntern)
+        choose()
+    })
+}
+
+function createHTML() {
+    fs.writeFile(outputPath, render(employees), (error, data) =>
+    error ? console.error(error) : console.log(data));
 }
 
 askManager()
